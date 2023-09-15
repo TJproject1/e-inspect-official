c764dabc48e3696dcda25cd63fd1db9fb23714d7
Brad <klausmickelson52@gmail.com>
2023-09-15 09:44:47 +0100

    chore: add face recognition

--- src/hooks/useStudentValidator.js
+++ src/hooks/useStudentValidator.js
@@ -1,11 +1,4 @@
-import {
-  collection,
-  doc,
-  getDoc,
-  getDocs,
-  query,
-  where,
-} from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { useState } from "react";
 import { db } from "../../firebase";
 
